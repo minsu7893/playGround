980163b19675b2c1d2a77b7038c18d933d6d200f
minsu <en9974@naver.com>
2024-09-26 00:32:47 +0900

    first commit

---
+++ src/test/java/com/playGround/playGround/PlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playGround.playGround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
