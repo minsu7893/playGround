f0e2410f3b7f3ff702f0cc3ce9a49b81f101c188
minsu <en9974@naver.com>
2024-09-29 00:13:33 +0900

    Add @CrossOrigin annotation to playGroundController.java

--- src/main/java/com/playGround/playGround/Controller/playGroundController.java
+++ src/main/java/com/playGround/playGround/Controller/playGroundController.java
@@ -1,5 +1,6 @@
 package com.playGround.playGround.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
