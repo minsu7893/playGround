ec609a97702601e513782866a21404db8c42640e
minsu <en9974@naver.com>
2024-10-03 00:02:08 +0900

    로그인 페이지 경로 설정

--- src/main/java/com/playGround/Security/SecurityConfig.java
+++ src/main/java/com/playGround/Security/SecurityConfig.java
@@ -1,29 +1,35 @@
 package com.playGround.Security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
+    /**
+     * @param http
+     * @return
+     * @throws Exception
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/index.html").authenticated() // index.html에 인증 요구
+                        .requestMatchers("/index.html").authenticated()  // index.html에만 인증 요구
                         .anyRequest().permitAll()  // 나머지 요청은 모두 허용
                 )
                 .formLogin((form) -> form
                         .loginPage("/login.html")  // 커스텀 로그인 페이지 경로
-                        .defaultSuccessUrl("/index.html", true)
+                        .defaultSuccessUrl("/index.html", true)  // 로그인 성공 시 index.html로 리다이렉트
                         .permitAll()  // 로그인 페이지는 누구나 접근 가능
                 )
-                .logout(LogoutConfigurer::permitAll);  // 로그아웃 허용
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/login.html")  // 로그아웃 성공 시 login.html로 리다이렉트
+                        .permitAll()
+                );
 
-        return http.build();
+        return http.build();  // 반드시 http.build()로 SecurityFilterChain 반환
     }
 }
-
