69ae97ac13b3aed3b05197a4a2cda701b92e0c68
minsu <en9974@naver.com>
2024-11-03 01:13:03 +0900

    코트 수정

--- src/main/java/com/playGround/model/Ssh.java
+++ src/main/java/com/playGround/model/Ssh.java
@@ -3,24 +3,20 @@ package com.playGround.model;
 import java.util.Date;
 
 public class Ssh {
+    private Date sshAcceptTime;
 
-    private final SshMapper sshMapper;
-
-    @Autowired
-    public SshService(SshMapper sshMapper) {
-        this.sshMapper = sshMapper;
+    public Date getSshAcceptTime() {
+        return sshAcceptTime;
     }
 
-    public void insertCurrentTime() {
-        Ssh ssh = new Ssh();
-        ssh.setSshAcceptTime(new Date());  // 현재 시간을 설정합니다.
-        
-        int result = sshMapper.insertSsh(ssh);  // 데이터베이스에 삽입합니다.
-        
-        if (result > 0) {
-            return "200";
-        } else {
-            return "";
-        }
+    public void setSshAcceptTime(Date sshAcceptTime) {
+        this.sshAcceptTime = sshAcceptTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Ssh{" +
+                "sshAcceptTime=" + sshAcceptTime +
+                '}';
     }
 }
