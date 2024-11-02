71743528229eed974ea5e0585652a19eb516a893
minsu <en9974@naver.com>
2024-11-03 01:19:13 +0900

    test

--- src/main/java/com/playGround/mapper/SshMapper.java
+++ src/main/java/com/playGround/mapper/SshMapper.java
@@ -1,6 +1,6 @@
 package com.playGround.mapper;
 
-import com.playGround.model.Ssh;
+import com.playGround.model.Ssh;  // 올바른 패키지 경로로 수정
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Insert;
@@ -9,9 +9,9 @@ import org.apache.ibatis.annotations.Insert;
 public interface SshMapper {
     // 데이터 조회 (기본 키로 조회)
     @Select("SELECT ssh_accept_time FROM ssh WHERE ssh_accept_time = #{sshAcceptTime}")
-    Ssh selectByAcceptTime(Date sshAcceptTime);
+    Ssh selectByAcceptTime(java.util.Date sshAcceptTime);  // java.util.Date로 명시
 
     // 데이터 삽입
     @Insert("INSERT INTO ssh (ssh_accept_time) VALUES (#{sshAcceptTime})")
     int insertSsh(Ssh ssh);
-}
+} 
